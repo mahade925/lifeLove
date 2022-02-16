08c9ca0b8d05c95b20b830b9ff5cf433b985cc5f
mahade925 <learnwithhasan2@gmail.com>
2022-02-16 23:41:20 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './Pages/Home/Home/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
